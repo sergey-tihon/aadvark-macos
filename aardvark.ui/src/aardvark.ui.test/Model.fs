0c7f9b889b7990c49646de78a9b9630573d5603c
Sergey Tihon <sergey.tihon@gmail.com>
2021-08-28 20:00:54 +0200

    template projects

---
+++ aardvark.ui/src/aardvark.ui.test/Model.fs
@@ -0,0 +1,19 @@
+﻿namespace aardvark.ui.Model
+
+open System
+open FSharp.Data.Adaptive
+
+open Aardvark.Base
+open Aardvark.UI.Primitives
+open Adaptify
+
+type Primitive =
+    | Box
+    | Sphere
+
+[<ModelType>]
+type Model =
+    {
+        currentModel    : Primitive
+        cameraState     : CameraControllerState
+    }
